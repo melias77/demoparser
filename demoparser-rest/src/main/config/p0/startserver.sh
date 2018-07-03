6f5770befb8b6f52074d32d048f04159df7742e9
Morten Bertelsen <mnb@kmd.dk>
2018-07-03 16:36:06 +0000

    clean up

--- demoparser-rest/src/main/config/p0/startserver.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -Xms1024m -Xmx3072m -Xss512k -jar target/demospringboot-rest.jar  --spring.config.location=classpath:/application.properties,file:target/config/local/config/application-override.properties 
